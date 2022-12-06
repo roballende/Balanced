a1c36b11c99557459cfac8aa3404414d293b8550
ZhiLeong <saber_zhileong@hotmail.com>
2022-12-06 16:10:46 -0500

    update on json

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-fetch()
+fetch("http://localhost:3000/Foods")
+    .then(resp => resp.json())
+    .then(foodArray => {renderArray(foodArray)})
+    
+//DOM Selector
+
+
+//This will show meat, grain, vegetables in table
+
+
+//Functions
+
