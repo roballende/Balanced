6e6c11b96cca54bdda91b1988494d39ee01d6174
ZhiLeong <saber_zhileong@hotmail.com>
2022-12-07 16:27:30 -0500

    done with project requirement

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ fetch("http://localhost:3000/Foods")
     .then(foodArray => {
         renderArray(foodArray)
         showImage(foodArray[0])
-        showImage1(foodArray[5])
-        showImage2(foodArray[10])
+        showImage(foodArray[5])
+        showImage(foodArray[10])
     })
-   
+
 //DOM Selector
 const foodTitle = document.querySelector(".displayName")
 const foodWeight = document.querySelector(".displayWeight")
@@ -17,74 +17,180 @@ const vegetableImage = document.querySelector("#vegetable-image")
 const proteinCalorieCount = document.querySelector("#protein-form")
 const grainCalorieCount = document.querySelector("#grain-form")
 const vegetablesCalorieCount = document.querySelector("#vegetable-form")
-const totalCalorie = document.querySelector("#total-calorie")
+const totalCalorie = document.querySelector("#calorie-holder")
+let form = document.querySelector("#dropdown-form")
+let mealCalorie = []
 
-//This will show meat, grain, vegetables in table
 
-
-//Item List
+    
+// Item List
 function renderArray(foodArray){
-    //Protein Column
+    //Food Column
     foodArray.forEach(food =>{
-        let proteinName = document.createElement("h3")
-        proteinName.textContent = food.fields.item_name_meat
-        const proteinContainer = document.querySelector(".protein-list")
-        proteinContainer.append(proteinName)
-        proteinName.addEventListener('click', () => showImage(food))
-        proteinName.addEventListener('mouseover', () => showDetails(food))
-        proteinName.addEventListener('click', () => proteinCalorie(food))
-    })
-    //Grain Column
-    foodArray.forEach(food =>{
-        let grainName = document.createElement("h3")
-        grainName.textContent = food.fields.item_name_grain
-        const grainContainer = document.querySelector(".grain-list")
-        grainContainer.append(grainName)
-        grainName.addEventListener('click', () => showImage1(food))
-        grainName.addEventListener('mouseover', () => showDetails1(food))
-    })
-    //Vegetables Column
-    foodArray.forEach(food =>{
-        let vegetablesName = document.createElement("h3")
-        vegetablesName.textContent = food.fields.item_name_vegetables
-        const vegetablesContainer = document.querySelector(".vegetables-list")
-        vegetablesContainer.append(vegetablesName)
-        vegetablesName.addEventListener('click', () => showImage2(food))
-        vegetablesName.addEventListener('mouseover', () => showDetails2(food))
+        if (food.fields.foodtype == "meat"){
+            let proteinName = document.createElement("h3")
+            proteinName.textContent = food.fields.item_name
+            const proteinContainer = document.querySelector(".protein-list")
+            proteinContainer.append(proteinName)
+            proteinName.addEventListener('click', () => showImage(food))
+            proteinName.addEventListener('mouseover', () => showDetails(food))
+        }
+        else if (food.fields.foodtype == "grain"){
+            let grainName = document.createElement("h3")
+            grainName.textContent = food.fields.item_name
+            const grainContainer = document.querySelector(".grain-list")
+            grainContainer.append(grainName)
+            grainName.addEventListener('click', () => showImage(food))
+            grainName.addEventListener('mouseover', () => showDetails(food))
+        }
+        else if (food.fields.foodtype == "vegetables"){
+            let vegetablesName = document.createElement("h3")
+            vegetablesName.textContent = food.fields.item_name
+            const vegetablesContainer = document.querySelector(".vegetables-list")
+            vegetablesContainer.append(vegetablesName)
+            vegetablesName.addEventListener('click', () => showImage(food))
+            vegetablesName.addEventListener('mouseover', () => showDetails(food))
+        }
     })
 }
+
+
+// Click addEventListener
+function showImage(food){
+    if (food.fields.foodtype == "meat"){
+        proteinImage.src = food.fields.images
+        proteinCalorieCount.textContent = food.fields.nf_calories
+    }
+    else if (food.fields.foodtype == "grain"){
+        grainImage.src = food.fields.images 
+        grainCalorieCount.textContent = food.fields.nf_calories
+    }
+    else if (food.fields.foodtype == "vegetables"){
+        vegetableImage.src = food.fields.images 
+        vegetablesCalorieCount.textContent = food.fields.nf_calories
+    }
+}
+
 
 //Mouseover addEventListener
 function showDetails(food){
-    foodTitle.textContent = food.fields.item_name_meat
-    foodWeight.textContent = food.fields.nf_serving_weight_grams
-    foodCalories.textContent = food.fields.nf_calories
+    if (food.fields.foodtype == "meat"){
+        foodTitle.textContent = food.fields.item_name
+        foodWeight.textContent = food.fields.nf_serving_weight_grams
+        foodCalories.textContent = food.fields.nf_calories
+    }
+    else if (food.fields.foodtype == "grain"){
+        foodTitle.textContent = food.fields.item_name
+        foodWeight.textContent = food.fields.nf_serving_weight_grams
+        foodCalories.textContent = food.fields.nf_calories
+    }
+    else if (food.fields.foodtype == "vegetables"){
+        foodTitle.textContent = food.fields.item_name
+        foodWeight.textContent = food.fields.nf_serving_weight_grams
+        foodCalories.textContent = food.fields.nf_calories
+    }
 }
-function showDetails1(food){
-    foodTitle.textContent = food.fields.item_name_grain
-    foodWeight.textContent = food.fields.nf_serving_weight_grams
-    foodCalories.textContent = food.fields.nf_calories
-}
-function showDetails2(food){
-    foodTitle.textContent = food.fields.item_name_vegetables
-    foodWeight.textContent = food.fields.nf_serving_weight_grams
-    foodCalories.textContent = food.fields.nf_calories
-}
-//Click addEventListener
-function showImage(food){
-    proteinImage.src = food.fields.images 
-    proteinCalorieCount.textContent = food.fields.nf_calories
-}
-function showImage1(food){
-    grainImage.src = food.fields.images
-    grainCalorieCount.textContent = food.fields.nf_calories
-}
-function showImage2(food){
-    vegetableImage.src = food.fields.images
-    vegetablesCalorieCount.textContent = food.fields.nf_calories
-}
+
 
 //Total Up 3 kind of food
+form.addEventListener('submit', calorieCalculator)
+function calorieCalculator(e) {
+    e.preventDefault()
+
+    if (e.target.servingsize.value == "Snack"){
+    mealCalorie = parseInt(proteinCalorieCount.textContent) + parseInt(grainCalorieCount.textContent) + parseInt(vegetablesCalorieCount.textContent)
+    }
+    else if (e.target.servingsize.value == "Meal"){
+    mealCalorie = ((parseInt(proteinCalorieCount.textContent) + parseInt(grainCalorieCount.textContent) + parseInt(vegetablesCalorieCount.textContent))*2)
+    }
+    else if (e.target.servingsize.value == "Feast"){
+    mealCalorie = ((parseInt(proteinCalorieCount.textContent) + parseInt(grainCalorieCount.textContent) + parseInt(vegetablesCalorieCount.textContent))*3)
+    }
+    totalCalorie.textContent = mealCalorie
+    
+    
+}
+
+// // Item List
+// function renderArray(foodArray){
+//     //Protein Column
+//     foodArray.forEach(food =>{
+//         let proteinName = document.createElement("h3")
+//         proteinName.textContent = food.fields.item_name_meat
+//         const proteinContainer = document.querySelector(".protein-list")
+//         proteinContainer.append(proteinName)
+//         proteinName.addEventListener('click', () => showImage(food))
+//         proteinName.addEventListener('mouseover', () => showDetails(food))
+        
+//     })
+//     //Grain Column
+//     foodArray.forEach(food =>{
+//         let grainName = document.createElement("h3")
+//         grainName.textContent = food.fields.item_name_grain
+//         const grainContainer = document.querySelector(".grain-list")
+//         grainContainer.append(grainName)
+//         grainName.addEventListener('click', () => showImage1(food))
+//         grainName.addEventListener('mouseover', () => showDetails1(food))
+//     })
+//     //Vegetables Column
+//     foodArray.forEach(food =>{
+//         let vegetablesName = document.createElement("h3")
+//         vegetablesName.textContent = food.fields.item_name_vegetables
+//         const vegetablesContainer = document.querySelector(".vegetables-list")
+//         vegetablesContainer.append(vegetablesName)
+//         vegetablesName.addEventListener('click', () => showImage2(food))
+//         vegetablesName.addEventListener('mouseover', () => showDetails2(food))
+//     })
+// }
 
 
+// // Click addEventListener
+// function showImage(food){
+//     proteinImage.src = food.fields.images
+//     proteinCalorieCount.textContent = food.fields.nf_calories
+// }
+// function showImage1(food){
+//     grainImage.src = food.fields.images 
+//     grainCalorieCount.textContent = food.fields.nf_calories
+// }
+// function showImage2(food){
+//     vegetableImage.src = food.fields.images 
+//     vegetablesCalorieCount.textContent = food.fields.nf_calories
+// }
 
+
+// //Mouseover addEventListener
+// function showDetails(food){
+//     foodTitle.textContent = food.fields.item_name_meat
+//     foodWeight.textContent = food.fields.nf_serving_weight_grams
+//     foodCalories.textContent = food.fields.nf_calories
+// }
+// function showDetails1(food){
+//     foodTitle.textContent = food.fields.item_name_grain
+//     foodWeight.textContent = food.fields.nf_serving_weight_grams
+//     foodCalories.textContent = food.fields.nf_calories
+// }
+// function showDetails2(food){
+//     foodTitle.textContent = food.fields.item_name_vegetables
+//     foodWeight.textContent = food.fields.nf_serving_weight_grams
+//     foodCalories.textContent = food.fields.nf_calories
+// }
+
+
+// //Total Up 3 kind of food
+// form.addEventListener('submit', calorieCalculator)
+// function calorieCalculator(e) {
+//     e.preventDefault()
+
+//     if (e.target.servingsize.value == "Snack"){
+//     mealCalorie = parseInt(proteinCalorieCount.textContent) + parseInt(grainCalorieCount.textContent) + parseInt(vegetablesCalorieCount.textContent)
+//     }
+//     else if (e.target.servingsize.value == "Meal"){
+//     mealCalorie = ((parseInt(proteinCalorieCount.textContent) + parseInt(grainCalorieCount.textContent) + parseInt(vegetablesCalorieCount.textContent))*2)
+//     }
+//     else if (e.target.servingsize.value == "Feast"){
+//     mealCalorie = ((parseInt(proteinCalorieCount.textContent) + parseInt(grainCalorieCount.textContent) + parseInt(vegetablesCalorieCount.textContent))*3)
+//     }
+//     totalCalorie.append(mealCalorie)
+    
+// }
